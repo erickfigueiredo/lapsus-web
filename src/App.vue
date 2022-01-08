a77f21b06aefa0255269aca45c3b23eca4b50604
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-01-07 23:45:16 -0300

    CREATED - Nova scrollbar mais moderna

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,25 @@
 </template>
 
 <style>
-  textarea {
-    resize: none;
-  }
+::-webkit-scrollbar {
+  width: 12px;               /* width of the entire scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1;        /* color of the tracking area */
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #cecece;    /* color of the scroll thumb */
+  border-radius: 20px;       /* roundness of the scroll thumb */
+  border: 2px solid #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #505f77;
+}
+
+textarea {
+  resize: none;
+}
 </style>
