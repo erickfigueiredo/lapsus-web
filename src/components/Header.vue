315df8f28e203c790c526e3d6eb1564686cc0550
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-02-08 13:54:58 -0300

    UPDATED - Adição de foto de perfil ao botão de seleção do cabecalho

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,61 +4,64 @@
   shadow-md"
   >
     <div class=" flex my-auto py-2">
-      <!-- Mudar a tag button -->
       <button class="ml-2 lg:hidden" @click="isActiveNav = true">
         <font-awesome :icon="['fas', 'bars']" class="text-2xl text-lemon-400 align-middle" />
       </button>
-      <!-- Mudar a tag button -->
-      <div class="ml-auto my-auto">
-        <!-- Mudar isso de escrever o nome e a questão da rota -->
-        <span class="text-white align-middle"> Olá, {{ $store.getters["userName"] }} </span>
-        <button
-          class="ml-4 bg-white rounded-full w-10 h-10 shadow-md mr-4 lg:mr-12"
-          @click="isActiveMenu = !isActiveMenu"
-        >
-          <font-awesome
-            :icon="['fas', 'user-circle']"
-            class="text-3xl text-cerulean-700 align-middle"
-          />
-        </button>
+      <div class="flex ml-auto my-auto items-center">
+        <span v-if="$store.getters['userName']" class="text-white align-middle">
+          Olá, {{ $store.getters["userName"] }}
+        </span>
+          <button
+            class="ml-4 bg-white rounded-full w-10 h-10 shadow-md mr-4 lg:mr-12"
+            @click="isActiveMenu = !isActiveMenu"
+          >
+            <img
+              v-if="isLoggedIn"
+              :src="profilePic"
+              :alt="userName"
+              class="rounded-full p-0.5"
+            />
+            <font-awesome
+              v-else
+              :icon="['fas', 'user-circle']"
+              class="text-3xl text-cerulean-700 align-middle"
+            />
+          </button>
+
       </div>
     </div>
   </header>
   <div v-if="isActiveMenu" class="absolute top-12 right-4 shadow-md rounded-lg z-20 lg:right-12">
     <div class="absolute -top-2 right-7 bg-white h-4 w-4 transform rotate-45" />
-    <section class="bg-white rounded-lg text-gray-700">
-      <ul v-if="$store.getters.isLoggedIn">
-        <li
-          class="px-6 py-2 rounded-t-lg hover:bg-gray-400 hover:text-white transition
-        duration-200"
-        >
-          <router-link to="/perfil" @click="closeMenu">Perfil</router-link>
+    <section class="bg-white rounded-lg">
+      <ul>
+        <template v-if="isLoggedIn">
+          <li class="px-6 py-2">
+            <router-link to="/perfil" @click="closeMenu">Perfil</router-link>
+          </li>
+          <li v-if="userType === 'A'" class="px-6 py-2">
+            <router-link to="/configuracoes" @click="closeMenu">Configurações</router-link>
+          </li>
+        </template>
+        <li v-else class="px-6 py-2">
+          <router-link to="/cadastro" @click="closeMenu">Cadastrar-me</router-link>
         </li>
-        <li class="px-6 py-2 hover:bg-gray-400 hover:text-white transition duration-200">
-          <router-link to="/configuracoes" @click="closeMenu">Configurações</router-link>
-        </li>
-        <li
-          class="px-6 block cursor-pointer py-2 text-red-500 hover:bg-red-500 rounded-b-lg
-          hover:text-white font-semibold transition duration-200"
-          @click="logout"
-        >
-          Sair
-        </li>
-      </ul>
-      <ul v-else>
-        <li>
-          <router-link to="/cadastro" class="block px-6 p-2">Cadastre-se</router-link>
-        </li>
-        <li>
-          <router-link to="/login" class="block px-6 p-2 text-red-500 hover:bg-red-500 rounded-b-lg
-          hover:text-white font-semibold transition duration-200">Sair</router-link>
-        </li>
+        <router-link to="" @click="logout">
+          <li
+            class="px-6 py-2 text-red-500 hover:bg-red-500 rounded-b-lg hover:text-white
+          font-semibold"
+          >
+            Sair
+          </li>
+        </router-link>
       </ul>
     </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import useMenus from '../hooks/useMenus';
 
 export default {
@@ -67,6 +70,9 @@ export default {
 
     return { isActiveNav, isActiveMenu };
   },
+  computed: {
+    ...mapGetters(['isLoggedIn', 'userType', 'userName', 'profilePic']),
+  },
   methods: {
     closeMenu() {
       this.isActiveMenu = false;
@@ -74,7 +80,7 @@ export default {
     logout() {
       this.closeMenu();
       this.$store.dispatch('logout');
-      this.$router.replace('/login');
+      this.$router.push('/login');
     },
   },
 };
