dc65f44774ce584ed21b322eb54f90ef423be7ed
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-03-03 11:08:33 -0300

    UPDATED - Atualização de página não encontrada

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,14 +3,10 @@
     <section class="mx-auto my-auto text-white text-center">
       <h1 class="text-4xl md:text-8xl text-lemon-400">404</h1>
       <p class="my-4 text-2xl md:text-4xl truncate">
-        {{
-          $route.redirectedFrom?.params.notFound
-            ? `"${$route.redirectedFrom.params.notFound}"`
-            : "Ops..."
-        }}
+        Ops...
       </p>
-      <p class="mb-12 text-2xl md:text-4xl">
-        Não existe!
+      <p class="my-4 text-2xl md:text-4xl truncate">
+        A página informada não existe!
       </p>
       <router-link to="/" exact class="font-bold">
         <p class="text-center">voltar para a Home</p>
