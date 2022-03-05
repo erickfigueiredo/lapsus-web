de15d9fb9c6c3908ef6497d7c8295c42194e3168
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-03-05 15:57:08 -0300

    CREATED - Gráfico de Donut

---
+++ src/components/charts/DoughnutChart.vue
@@ -0,0 +1,37 @@
+<script>
+import { Doughnut } from 'vue3-chart-v2';
+
+export default {
+  name: 'PieChart',
+  extends: Doughnut,
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+    legend: {
+      type: Array,
+      required: true,
+    },
+    colours: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  mounted() {
+    this.renderChart({
+      labels: this.legend,
+      datasets: [
+        {
+          backgroundColor: this.colours,
+          data: this.data,
+        },
+      ],
+    }, {
+      responsive: true,
+      maintainAspectRatio: false,
+    });
+  },
+};
+</script>
